6143971513776a00972c3bf572baaade1f2e3ade
fjutnetworkcxl <cxloong2702@foxmail.com>
2019-09-23 17:49:36 +0800

    代码更新

--- src/configs/hrefCfg.js
+++ src/configs/hrefCfg.js
@@ -1,7 +1,7 @@
 const {systemApi} = Utils;
 const hrefCfg = {
     dev: {
-        apiPath: 'https://csapi.zzha.vip/mall',
+        apiPath: 'https://yapi.zzha.vip/mall',
         apiShopPath: 'https://csapi.zzha.vip/rui',
         scan: 'https://csapp.zzha.vip/mall.html#'
     },
