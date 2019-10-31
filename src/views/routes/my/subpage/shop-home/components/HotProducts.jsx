6ccb901665ca8d0477e2ae09c432c76e9d9dc09c
fjutnetworkcxl <cxloong2702@foxmail.com>
2019-10-31 18:05:50 +0800

    模板公用组件优化

--- src/views/routes/my/subpage/shop-home/components/HotProducts.jsx
+++ src/views/routes/my/subpage/shop-home/components/HotProducts.jsx
@@ -7,21 +7,23 @@ export default class HotProducts extends BaseComponent {
         return (
             <div className="hotImgShow">
                 {
-                    hotSellList.map(item => (
-                        <div className="top" key={item}>
-                            <img onClick={() => goToGoods(content[`sort${sort}_pr${item - mode2}_id`])} src={picurl[item]} alt=""/>
-                            <div className="priceInfo">
-                                <div className="infoLeft">
-                                    <p className="shopTitleSmall">{content[`sort${sort}_pr${item - mode2}_title1`]}</p>
-                                    <div className="shopPeiceSmall">
-                                        <span className="money-ZH">￥</span>
-                                        <span className="money-now">{content[`sort${sort}_pr${item - mode2}_title2`]}</span>
-                                        <span className="money-before">{content[`sort${sort}_pr${item - mode2}_title3`]}</span>
+                    (content && picurl && hotSellList && hotSellList.leng > 0) ? (
+                        hotSellList.map(item => (
+                            <div className="top" key={item}>
+                                <img onClick={() => goToGoods(content[`sort${sort}_pr${item - mode2}_id`])} src={picurl[item]} alt=""/>
+                                <div className="priceInfo">
+                                    <div className="infoLeft">
+                                        <p className="shopTitleSmall">{content[`sort${sort}_pr${item - mode2}_title1`]}</p>
+                                        <div className="shopPeiceSmall">
+                                            <span className="money-ZH">￥</span>
+                                            <span className="money-now">{content[`sort${sort}_pr${item - mode2}_title2`]}</span>
+                                            <span className="money-before">{content[`sort${sort}_pr${item - mode2}_title3`]}</span>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    ))
+                        ))
+                    ) : null
                 }
             </div>
         );
--- src/views/routes/my/subpage/shop-home/components/Products.jsx
+++ src/views/routes/my/subpage/shop-home/components/Products.jsx
@@ -7,21 +7,23 @@ export default class Products extends BaseComponent {
         return (
             <div className="newImgShow">
                 {
-                    newSellList.map(item => (
-                        <div className="bottomImgLeft" key={item}>
-                            <div className="marginBot">
-                                <img onClick={() => goToGoods(content[`sort${sort}_pr${item - mode2}_id`])} src={picurl[item]} alt=""/>
-                                <div className="margin-bot-box">
-                                    <div className="margin-bot-content-top">{content[`sort${sort}_pr${item - mode2}_title1`]}</div>
-                                    <div className="margin-bot-content-bot">
-                                        <span className="money-ZH">￥</span>
-                                        <span className="money-now">{content[`sort${sort}_pr${item - mode2}_title2`]}</span>
-                                        <span className="money-before">{content[`sort${sort}_pr${item - mode2}_title3`]}</span>
+                    (content && picurl && newSellList && newSellList.leng > 0) ? (
+                        newSellList.map(item => (
+                            <div className="bottomImgLeft" key={item}>
+                                <div className="marginBot">
+                                    <img onClick={() => goToGoods(content[`sort${sort}_pr${item - mode2}_id`])} src={picurl[item]} alt=""/>
+                                    <div className="margin-bot-box">
+                                        <div className="margin-bot-content-top">{content[`sort${sort}_pr${item - mode2}_title1`]}</div>
+                                        <div className="margin-bot-content-bot">
+                                            <span className="money-ZH">￥</span>
+                                            <span className="money-now">{content[`sort${sort}_pr${item - mode2}_title2`]}</span>
+                                            <span className="money-before">{content[`sort${sort}_pr${item - mode2}_title3`]}</span>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    ))
+                        ))
+                    ) : null
                 }
             </div>
         );
