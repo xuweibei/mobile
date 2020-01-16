24c87c3522ae32f4973bdd3f3dc874fc8f985489
fjutnetworkcxl <cxloong2702@foxmail.com>
2020-01-16 13:47:29 +0800

    核销失败缺省

--- src/views/routes/my/subpage/inspect/consumer/Consumer.jsx
+++ src/views/routes/my/subpage/inspect/consumer/Consumer.jsx
@@ -4,6 +4,7 @@ import {connect} from 'react-redux';
 import {myActionCreator} from '../../../actions/index';
 import './Consumer.less';
 import AppNavBar from '../../../../../common/navbar/NavBar';
+import FailWrite from '../fail-write/FailWrite';
 
 const {urlCfg} = Configs;
 const {appHistory, getUrlParam, native} = Utils;
@@ -11,7 +12,8 @@ class Consumer extends BaseComponent {
     state = {
         list: {},
         order: [],
-        val: decodeURI(getUrlParam('val', encodeURI(this.props.location.search)))
+        val: decodeURI(getUrlParam('val', encodeURI(this.props.location.search))),
+        codeStatus: 1
     }
 
     componentDidMount() {
@@ -27,7 +29,13 @@ class Consumer extends BaseComponent {
             if (res && res.status === 0) {
                 this.setState({
                     list: res.data,
-                    order: res.data.pr_list
+                    order: res.data.pr_list,
+                    codeStatus: 1
+                });
+            } else {
+                this.setState({
+                    errCode: res.error_code,
+                    codeStatus: 0
                 });
             }
         });
@@ -58,59 +66,61 @@ class Consumer extends BaseComponent {
     }
 
     render() {
-        const {list, order} = this.state;
+        const {list, order, codeStatus, errCode} = this.state;
         return (
-            <div className="consumer">
-                <AppNavBar title="确认核销" goBackModal={this.goBackModal}/>
-                <div className="picture">
-                    <img src={list.avatarUrl} alt=""/>
-                    <span>{list.nickname}</span>
-                </div>
-                <div className="indent-box">
-                    <div className="indent">
-                        <div>
-                            <div className="serial">
-                                <span>订单编号：{list.order_no}</span>
-                            </div>
-                            {list.affective_type === '1' ? (
-                                <div className="serial">使用时间：长期有效</div>
-                            ) : (
-                                <div className="serial">使用时间：{list.white_start_time + ' - ' + list.white_end_time}</div>
-                            )}
-                            {
-                                order.map(item => (
-                                    <div className="content" key={item.pr_id}>
-                                        <img src={item.pr_picpath} alt=""/>
-                                        <div className="content-right">
-                                            <div className="value">
-                                                <span>{item.pr_title}</span>
-                                                <span>￥{item.price}</span>
-                                            </div>
-                                            <div className="specification">
-                                                <span>{item.property_content[0]}</span>
-                                                <span>{item.property_content[1]}</span>
-                                                <span>规格</span>
-                                            </div>
-                                            <div className="accounts">
-                                                <span>记账量：{item.deposit}</span>
-                                                <span>x{item.num}</span>
+            codeStatus ? (
+                <div className="consumer">
+                    <AppNavBar title="确认核销" goBackModal={this.goBackModal}/>
+                    <div className="picture">
+                        <img src={list.avatarUrl} alt=""/>
+                        <span>{list.nickname}</span>
+                    </div>
+                    <div className="indent-box">
+                        <div className="indent">
+                            <div>
+                                <div className="serial">
+                                    <span>订单编号：{list.order_no}</span>
+                                </div>
+                                {list.affective_type === '1' ? (
+                                    <div className="serial">使用时间：长期有效</div>
+                                ) : (
+                                    <div className="serial">使用时间：{list.white_start_time + ' - ' + list.white_end_time}</div>
+                                )}
+                                {
+                                    order.map(item => (
+                                        <div className="content" key={item.pr_id}>
+                                            <img src={item.pr_picpath} alt=""/>
+                                            <div className="content-right">
+                                                <div className="value">
+                                                    <span>{item.pr_title}</span>
+                                                    <span>￥{item.price}</span>
+                                                </div>
+                                                <div className="specification">
+                                                    <span>{item.property_content[0]}</span>
+                                                    <span>{item.property_content[1]}</span>
+                                                    <span>规格</span>
+                                                </div>
+                                                <div className="accounts">
+                                                    <span>记账量：{item.deposit}</span>
+                                                    <span>x{item.num}</span>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                ))
-                            }
+                                    ))
+                                }
+                            </div>
+                            <p className="altogether">总记账量：<span>{list.all_deposit}</span></p>
+                            <p className="total">
+                                <span>共{list.pr_count}件商品</span>
+                                <span>合计：<span>￥{list.all_price}</span></span>
+                            </p>
                         </div>
-                        <p className="altogether">总记账量：<span>{list.all_deposit}</span></p>
-                        <p className="total">
-                            <span>共{list.pr_count}件商品</span>
-                            <span>合计：<span>￥{list.all_price}</span></span>
-                        </p>
+                    </div>
+                    <div className="pushbutton">
+                        <div onClick={this.sure}>确认核销</div>
                     </div>
                 </div>
-                <div className="pushbutton">
-                    <div onClick={this.sure}>确认核销</div>
-                </div>
-            </div>
+            ) : (<FailWrite errCode={errCode}/>)
         );
     }
 }
