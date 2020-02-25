24ffaf3d132cf2c6b7060b119343eb9e37a84052
xuweibei <1582986545@qq.com>
2020-02-25 17:51:48 +0800

    原生跳转login页调整

--- src/http/rxios.js
+++ src/http/rxios.js
@@ -8,7 +8,7 @@ import {defer} from 'rxjs/Observable/defer';
 import {Subject} from 'rxjs/Subject';
 import 'rxjs/add/operator/do';
 import {store} from '../redux/store';
-// import {native} from '../utils/native';
+import {native} from '../utils/native';
 
 import {baseActionCreator as actionCreator} from '../redux/baseAction';
 
@@ -39,8 +39,8 @@ axios.interceptors.response.use(
                 removeValue(LOCALSTORAGE.USER_TOKEN); // 清除token,localstorage
                 store.dispatch(actionCreator.setUserToken('')); // 清除redux的userToken
                 //重定向到原生登录页
-                // native('loginout');
-                window.DsBridge.call('loginout');
+                native('loginout');
+                // window.DsBridge.call('loginout');
             } else {
                 appHistory.push('/login');
             }
